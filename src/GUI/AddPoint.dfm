14d76d8c84793b8aa4fc7f7bfb25dd8821d5b473
kovarmi9 <kovarmi9@seznam.cz>
2025-08-01 07:18:51 +0200

    Updated AddPoint Form

--- src/GUI/AddPoint.dfm
+++ src/GUI/AddPoint.dfm
@@ -2,7 +2,7 @@ object Form6: TForm6
   Left = 0
   Top = 0
   Caption = 'P'#345'idat bod'
-  ClientHeight = 200
+  ClientHeight = 196
   ClientWidth = 407
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
@@ -14,9 +14,21 @@ object Form6: TForm6
   OnCreate = FormCreate
   OnShow = FormShow
   TextHeight = 15
-  object StringGrid1: TStringGrid
+  object lblWarning: TLabel
     Left = 8
     Top = 8
+    Width = 3
+    Height = 15
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -12
+    Font.Name = 'Segoe UI'
+    Font.Style = [fsBold]
+    ParentFont = False
+  end
+  object StringGrid1: TStringGrid
+    Left = 8
+    Top = 29
     Width = 391
     Height = 128
     ColCount = 6
@@ -26,8 +38,8 @@ object Form6: TForm6
     OnKeyPress = StringGrid1KeyPress
   end
   object btnOK: TButton
-    Left = 216
-    Top = 144
+    Left = 224
+    Top = 163
     Width = 75
     Height = 25
     Caption = 'OK'
@@ -35,8 +47,8 @@ object Form6: TForm6
     TabOrder = 1
   end
   object btnCancel: TButton
-    Left = 304
-    Top = 144
+    Left = 305
+    Top = 163
     Width = 75
     Height = 25
     Caption = 'Zru'#353'it'
--- src/GUI/AddPoint.pas
+++ src/GUI/AddPoint.pas
@@ -1,305 +1,9 @@
-﻿//unit AddPoint;
-//
-//interface
-//
-//uses
-//  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, Vcl.Graphics, System.Generics.Collections,
-//  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Grids, Vcl.Menus, System.Math, ComObj,
-//  StringGridValidationUtils, PointsUtilsSingleton, ValidationUtils, System.Classes, Point,
-//  Vcl.ComCtrls, Vcl.ToolWin, Vcl.ActnMan, Vcl.ActnCtrls, Vcl.ActnMenus,
-//  Vcl.ExtCtrls, System.IOUtils, Vcl.StdCtrls;
-//
-//type
-//  TForm6 = class(TForm)
-//    StringGrid1: TStringGrid;
-//    StatusBar1: TStatusBar;
-//    ControlBar1: TControlBar;
-//    MainMenu1: TMainMenu;
-//    File1: TMenuItem;
-//    File2: TMenuItem;
-//    SaveAs1: TMenuItem;
-//    SaveAs2: TMenuItem;
-//    Import1: TMenuItem;
-//    FromTXT1: TMenuItem;
-//    FromTXT2: TMenuItem;
-//    FromBinary1: TMenuItem;
-//    Import2: TMenuItem;
-//    oTXT1: TMenuItem;
-//    oTXT2: TMenuItem;
-//    oBinary1: TMenuItem;
-//    OpenDialog1: TOpenDialog;
-//    SaveDialog1: TSaveDialog;
-//    Button1: TButton;
-//    Button2: TButton;
-//    procedure FormCreate(Sender: TObject); // Procedura volaná při inicializaci formuláře
-//    procedure StringGrid1KeyPress(Sender: TObject; var Key: Char); // Procedura pro zpracování stisknutí klávesy
-//    procedure StringGrid1KeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
-//    procedure StringGrid1DrawCell(Sender: TObject; ACol, ARow: Integer; Rect: TRect; State: TGridDrawState);
-//    procedure UpdateCurrentDirectoryPath;
-//    procedure StringGrid1SelectCell(Sender: TObject; ACol, ARow: Integer; var CanSelect: Boolean);
-//  private
-//    { Private declarations }
-//  public
-//    { Public declarations }
-//  end;
-//
-//var
-//  Form6: TForm6;
-//
-//implementation
-//
-//{$R *.dfm}
-//
-//procedure TForm6.FormCreate(Sender: TObject); // Změněno z TForm2 na TForm3
-//var
-//  P: TPoint;
-//  i: Integer;
-//begin
-//// Nastavení sloupců a řádků pro StringGrid1 (tabulka pro zadávání souřadnic)
-//  StringGrid1.ColCount := 6; // Počet sloupců: Číslo bodu, X, Y, Z, Popis
-//  StringGrid1.RowCount := 2; // Minimálně 2 řádky (hlavička + 1 prázdný řádek pro vstup)
-//  StringGrid1.FixedRows := 1; // První řádek bude pevný (neměnný)
-//
-//  // Nastavení popisků sloupců (hlavičky) pro StringGrid1
-//  StringGrid1.Cells[0, 0] := 'Číslo bodu'; // Název sloupce 0
-//  StringGrid1.Cells[1, 0] := 'X';          // Název sloupce 1
-//  StringGrid1.Cells[2, 0] := 'Y';          // Název sloupce 2
-//  StringGrid1.Cells[3, 0] := 'Z';          // Název sloupce 3
-//  StringGrid1.Cells[4, 0] := 'Kvalita';    // Název sloupce 4
-//  StringGrid1.Cells[5, 0] := 'Popis';      // Název sloupce 4
-//
-//  // Nastavení výchozích hodnot v prvním řádku pro zadávání bodů
-//  StringGrid1.Cells[0, 1] := ''; // Číslo bodu (prázdná)
-//  StringGrid1.Cells[1, 1] := '';  // X souřadnice (prázdná)
-//  StringGrid1.Cells[2, 1] := '';  // Y souřadnice (prázdná)
-//  StringGrid1.Cells[3, 1] := '';  // Z souřadnice (prázdná)
-//  StringGrid1.Cells[4, 1] := '';  // Kvalita (prázdná)
-//  StringGrid1.Cells[5, 1] := '';  // Popis (prázdný)
-//
-//  // --- NOVO: naplníme grid existujícími body ze slovníku ---
-//  i := 1;  // začínáme na prvním datovém řádku
-//  for P in TPointDictionary.GetInstance.Values do
-//  begin
-//    // zajistíme dostatek řádků
-//    StringGrid1.RowCount := i + 1;
-//    // vyplníme sloupce 0..5
-//    StringGrid1.Cells[0, i] := IntToStr(P.PointNumber);
-//    StringGrid1.Cells[1, i] := FloatToStr(P.X);
-//    StringGrid1.Cells[2, i] := FloatToStr(P.Y);
-//    StringGrid1.Cells[3, i] := FloatToStr(P.Z);
-//    StringGrid1.Cells[4, i] := IntToStr(P.Quality);
-//    StringGrid1.Cells[5, i] := P.Description;
-//    Inc(i);
-//  end;
-//
-//  StringGrid1.Repaint;
-//
-//  // Přiřazení obslužných procedur pro události
-//  StringGrid1.OnKeyPress := StringGrid1KeyPress;
-//  StringGrid1.OnKeyDown := StringGrid1KeyDown;
-//  StringGrid1.OnDrawCell := StringGrid1DrawCell;
-//  StringGrid1.OnSelectCell := StringGrid1SelectCell;
-//
-//  // Aktualizace cesty
-//  UpdateCurrentDirectoryPath;
-//
-//end;
-//procedure TForm6.UpdateCurrentDirectoryPath;
-//begin
-//  if StatusBar1.Panels.Count > 0 then
-//    StatusBar1.Panels[0].Text := GetCurrentDir;
-//end;
-//
-//procedure TForm6.StringGrid1KeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
-//var
-//  PointNumber: Integer;
-//  X, Y, Z: Double;
-//  Quality: Integer;
-//  Description: string;
-//  NewPoint: TPoint;
-//begin
-//  if (Key = VK_RETURN) or (Key = VK_TAB) then
-//  begin
-//    Key := 0; // Zamezí dalšímu zpracování Enteru
-//
-//    // Vyhodnocení výrazu a převedení na číslo
-//    if StringGrid1.Col in [1, 2, 3] then
-//    begin
-//      try
-//        StringGrid1.Cells[StringGrid1.Col, StringGrid1.Row] := FloatToStr(EvaluateExpression(StringGrid1.Cells[StringGrid1.Col, StringGrid1.Row]));
-//      except
-//        on E: Exception do
-//          ShowMessage('Chyba ve výrazu: ' + E.Message);
-//      end;
-//    end;
-//
-//    // Přechod na další buňku
-//    if StringGrid1.Col < StringGrid1.ColCount - 1 then
-//    begin
-//      StringGrid1.Col := StringGrid1.Col + 1;
-//    end
-//    else
-//    begin
-//      // Pokud poslední sloupec, přechod na první sloupec dalšího řádku
-//      if StringGrid1.Row = StringGrid1.RowCount - 1 then
-//
-//      StringGrid1.Row := StringGrid1.Row;
-//      StringGrid1.Col := 0;
-//
-//      // Načtení hodnot
-//      PointNumber := StrToIntDef(StringGrid1.Cells[0, StringGrid1.Row - 1], -1);
-//      X := StrToFloatDef(StringGrid1.Cells[1, StringGrid1.Row - 1], NaN);
-//      Y := StrToFloatDef(StringGrid1.Cells[2, StringGrid1.Row - 1], NaN);
-//      Z := StrToFloatDef(StringGrid1.Cells[3, StringGrid1.Row - 1], NaN);
-//      Quality := StrToIntDef(StringGrid1.Cells[4, StringGrid1.Row - 1], -1);
-//      Description := StringGrid1.Cells[5, StringGrid1.Row - 1];
-//
-//      // Uložení do slovníku pomocí singletonu
-//      if (PointNumber <> -1) and (not IsNan(X)) and (not IsNan(Y)) and (not IsNan(Z)) then
-//      begin
-//        // Použití singletonu pro získání instance TPointDictionary
-//        TPointDictionary.GetInstance.AddPoint(TPoint.Create(PointNumber, X, Y, Z, Quality, Description));
-//
-//        // Kontrola, zda byl bod vložen a uložení do nového bodu
-//        if TPointDictionary.GetInstance.PointExists(PointNumber) then
-//        begin
-//          NewPoint := TPointDictionary.GetInstance.GetPoint(PointNumber);
-//          ShowMessage(Format('Bod %d byl vložen do ss: X=%.2f, Y=%.2f, Z=%.2f, Kvalita=%d, Popis=%s',
-//            [NewPoint.PointNumber, NewPoint.X, NewPoint.Y, NewPoint.Z, NewPoint.Quality, NewPoint.Description]));
-//        end
-//        else
-//          ShowMessage(Format('Bod %d nebyl vložen.', [PointNumber]));
-//      end
-//      else
-//        ShowMessage('Neplatná data, bod nebyl uložen.');
-//    end;
-//  end
-//  else if Key = VK_DELETE then
-//  begin
-//    StringGrid1.Cells[StringGrid1.Col, StringGrid1.Row] := ''; // Mazání obsahu buňky
-//  end;
-//end;
-//
-//procedure TForm6.StringGrid1KeyPress(Sender: TObject; var Key: Char);
-//begin
-//  HandleBackspace(StringGrid1, Key);
-//  ValidatePointNumber(StringGrid1, Key);
-//  ValidateCoordinates(StringGrid1, Key);
-//  ValidateQualityCode(StringGrid1, Key);
-//end;
-//
-//procedure TForm6.StringGrid1DrawCell(Sender: TObject; ACol, ARow: Integer;
-//  Rect: TRect; State: TGridDrawState);
-//begin
-//  with TStringGrid(Sender).Canvas do
-//  begin
-//    // hlavička = vždy šedá
-//    if ARow < TStringGrid(Sender).FixedRows then
-//      Brush.Color := clMenuBar
-//    else
-//      Brush.Color := clWindow;
-//
-//    FillRect(Rect);
-//
-//    // text
-//    TextRect(Rect, Rect.Left + 4, Rect.Top + 2,
-//      TStringGrid(Sender).Cells[ACol, ARow]);
-//  end;
-//end;
-//
-//procedure TForm6.StringGrid1SelectCell(Sender: TObject; ACol, ARow: Integer; var CanSelect: Boolean);
-//begin
-//  // Zablokuj výběr (a tedy i úpravu) hlavičky
-//  CanSelect := ARow <> 0;
-//end;
-//
-//end.
-
-//unit AddPoint;
-//
-//interface
-//
-//uses
-//  Vcl.Forms,
-//  Vcl.Grids,
-//  Vcl.StdCtrls,
-//  System.SysUtils,
-//  Point, Vcl.Dialogs, Vcl.Menus, Vcl.ExtCtrls, Vcl.ComCtrls, System.Classes,
-//  Vcl.Controls;  // vaše jednotka, kde je TPoint.Create(validace)
-//
-//type
-//  TForm6 = class(TForm)
-//    StringGrid1: TStringGrid;
-//    btnOK: TButton;
-//    btnCancel: TButton;
-//    procedure FormCreate(Sender: TObject);
-//  private
-//    { Private declarations }
-//  public
-//    /// <summary>
-//    /// Zobrazí dialog pro jedno zadání bodu.
-//    /// Vrátí True, pokud uživatel klikne OK,
-//    /// a out NewP vrátí validovaný bod.
-//    /// </summary>
-//    function Execute(PointNumber: Integer; out NewP: TPoint): Boolean;
-//  end;
-//
-//var
-//  Form6: TForm6;
-//
-//implementation
-//
-//{$R *.dfm}
-//
-//procedure TForm6.FormCreate(Sender: TObject);
-//begin
-//  // grid nastavíme na 6 sloupců (0=číslo, 1=X,2=Y,3=Z,4=Quality,5=Popis)
-//  StringGrid1.ColCount := 6;
-//  StringGrid1.RowCount := 2;
-//  StringGrid1.FixedRows := 1;
-//
-//  // hlavička
-//  StringGrid1.Cells[0,0] := 'Číslo bodu';
-//  StringGrid1.Cells[1,0] := 'X';
-//  StringGrid1.Cells[2,0] := 'Y';
-//  StringGrid1.Cells[3,0] := 'Z';
-//  StringGrid1.Cells[4,0] := 'Kvalita';
-//  StringGrid1.Cells[5,0] := 'Popis';
-//
-//  // tlačítka
-//  btnOK.ModalResult := mrOk;
-//  btnCancel.ModalResult := mrCancel;
-//end;
-//
-//function TForm6.Execute(PointNumber: Integer; out NewP: TPoint): Boolean;
-//begin
-//  // předešlete číslo bodu do první buňky
-//  StringGrid1.Cells[0,1] := IntToStr(PointNumber);
-//
-//  // ukažte formu
-//  Result := (ShowModal = mrOk);
-//  if not Result then
-//    Exit;
-//
-//  // sestavte TPoint (validace v konstruktoru)
-//  NewP := TPoint.Create(
-//    PointNumber,
-//    StrToFloatDef(StringGrid1.Cells[1,1], 0),
-//    StrToFloatDef(StringGrid1.Cells[2,1], 0),
-//    StrToFloatDef(StringGrid1.Cells[3,1], 0),
-//    StrToIntDef(  StringGrid1.Cells[4,1], 0),
-//    StringGrid1.Cells[5,1]
-//  );
-//end;
-//
-//end.
-
-unit AddPoint;
+﻿unit AddPoint;
 
 interface
 
 uses
-  Winapi.Windows, System.SysUtils, Vcl.Forms, Vcl.Grids, Vcl.StdCtrls, Vcl.Controls,
+  Winapi.Windows, System.SysUtils, Vcl.Forms, Vcl.Grids, Vcl.StdCtrls, Vcl.Controls, Vcl.Graphics,
   Point, System.Classes,
   StringGridValidationUtils, ValidationUtils,
   PointsManagement;
@@ -309,10 +13,13 @@ type
     StringGrid1: TStringGrid;
     btnOK: TButton;
     btnCancel: TButton;
+    lblWarning: TLabel;
     procedure FormCreate(Sender: TObject);
     procedure StringGrid1KeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
     procedure StringGrid1KeyPress(Sender: TObject; var Key: Char); // Procedura pro zpracování stisknutí klávesy
     procedure FormShow(Sender: TObject);
+    procedure StringGrid1DrawCell(Sender: TObject; ACol, ARow: Integer; Rect: TRect; State: TGridDrawState);
+    procedure StringGrid1Enter(Sender: TObject);
   private
     { Private declarations }
   public
@@ -335,6 +42,10 @@ procedure TForm6.FormCreate(Sender: TObject);
 var
   c: Integer;
 begin
+
+  //vykreslení hlavičky
+  StringGrid1.OnDrawCell := StringGrid1DrawCell;
+
   // Nastavíme grid: 6 sloupců, 2 řádky, 1 pevný
   StringGrid1.ColCount := 6;
   StringGrid1.RowCount := 2;
@@ -355,33 +66,17 @@ begin
   //Přepínaní pomocí enteru
   StringGrid1.OnKeyDown := StringGrid1KeyDown;
 
-end;
+  // Poskočení do první buňky po tabu
+  StringGrid1.OnEnter := StringGrid1Enter;
 
-//function TForm6.Execute(PointNumber: Integer; out NewP: TPoint): Boolean;
-//begin
-//  // Předešleme číslo bodu do gridu
-//  StringGrid1.Cells[0,1] := IntToStr(PointNumber);
-//
-//  // Zobrazíme modálně
-//  Result := (ShowModal = mrOk);
-//  if not Result then
-//    Exit;
-//
-//  // Vytvoříme validovaný bod (validace probíhá v konstruktoru TPoint.Create)
-//  NewP := TPoint.Create(
-//    PointNumber,
-//    StrToFloatDef(StringGrid1.Cells[1,1], 0.0),
-//    StrToFloatDef(StringGrid1.Cells[2,1], 0.0),
-//    StrToFloatDef(StringGrid1.Cells[3,1], 0.0),
-//    StrToIntDef(  StringGrid1.Cells[4,1], 0),
-//    StringGrid1.Cells[5,1]
-//  );
-//
-//end;
+end;
 
 function TForm6.Execute(PointNumber: Integer; out NewP: TPoint): Boolean;
 begin
   StringGrid1.Cells[0,1] := IntToStr(PointNumber);
+
+  // Zobrazím warning, pokud bod neexistuje
+  lblWarning.Caption := Format('Bod %d nebyl nalezen. Přejete si jej přidat?', [PointNumber]);
 
   Result := (ShowModal = mrOk);
   if not Result then
@@ -396,126 +91,77 @@ begin
     StringGrid1.Cells[5,1]
   );
 
-  // Přidej do slovníku
-  //TPointDictionary.GetInstance.AddPoint(NewP);
-
   // Pokud je PointsManagement (Form2) otevřený, refreshni grid
   if Assigned(Form2) and Form2.Visible then
     Form2.RefreshGrid;  // nebo vlastní RefreshGrid metoda
 end;
 
-//procedure TForm6.StringGrid1KeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
-//begin
-//  if Key = VK_RETURN then
-//  begin
-//    Key := 0;                        // zamezíme defaultnímu „beep“ či jinému zpracování
-//    with StringGrid1 do
-//    begin
-//      // pokud nejsem v posledním sloupci -> posuň se doprava
-//      if Col < ColCount - 1 then
-//        Col := Col + 1
-//      else
-//      begin
-//        // jinak přejdi na další řádek, první sloupec
-//        if Row < RowCount - 1 then
-//          Row := Row + 1;
-//        Col := 0;
-//      end;
-//      // spustíme režim editace v buňce
-//      EditorMode := True;
-//    end;
-//  end;
-//end;
-
-//procedure TForm6.StringGrid1KeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
-//begin
-//  if (Key = VK_RETURN) or (Key = VK_TAB) then
-//  begin
-//    Key := 0;  // zamezíme defaultnímu chování (beep, skok)
-//
-//    with StringGrid1 do
-//    begin
-//      // Pokud jsme v X, Y nebo Z -> převeď výraz na číslo
-//      if Col in [1, 2, 3] then
-//      begin
-//        try
-//          Cells[Col, Row] := FloatToStr(EvaluateExpression(Cells[Col, Row]));
-//        except
-//          on E: Exception do
-//            //ShowMessage('Chyba ve výrazu: ' + E.Message);
-//        end;
-//      end;
-//
-//      // Pohyb na další buňku
-//      if Col < ColCount - 1 then
-//        Col := Col + 1
-//      else
-//      begin
-//        if Row < RowCount - 1 then
-//          Row := Row + 1;
-//        Col := 0;
-//      end;
-//
-//      EditorMode := True;
-//    end;
-//  end
-//  else if Key = VK_DELETE then
-//  begin
-//    StringGrid1.Cells[StringGrid1.Col, StringGrid1.Row] := ''; // Mazání buňky
-//  end;
-//end;
-
 procedure TForm6.StringGrid1KeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+var
+  Step: Integer;
 begin
   if (Key = VK_RETURN) or (Key = VK_TAB) then
   begin
-    Key := 0;  // Zamezí defaultnímu chování (beep, automatický pohyb)
+    Key := 0; // zamezí defaultnímu chování
+
+    // Načti stav z Tagu StringGridu
+    Step := StringGrid1.Tag;
 
     with StringGrid1 do
     begin
-      // Pokud jsme ve sloupci X, Y nebo Z -> převeď matematický výraz na číslo
+      // Vyhodnoť výraz ve sloupcích X,Y,Z
       if Col in [1, 2, 3] then
       begin
         try
           Cells[Col, Row] := FloatToStr(EvaluateExpression(Cells[Col, Row]));
         except
-          on E: Exception do
-            //ShowMessage('Chyba ve výrazu: ' + E.Message);
+          on E: Exception do ;
         end;
       end;
 
-      // Pohyb na další buňku v rámci řádku 1 (cykluje jen v datovém řádku)
-      if Col < ColCount - 1 then
-        Col := Col + 1
+      // Jsme na posledním sloupci datového řádku
+      if (Col = ColCount - 1) and (Row = 1) then
+      begin
+        case Step of
+          0: begin
+               btnOK.SetFocus;
+               Step := 1;
+               StringGrid1.Tag := Step; // uložíme stav
+               Exit;
+             end;
+          1: begin
+               btnCancel.SetFocus;
+               Step := 2;
+               StringGrid1.Tag := Step;
+               Exit;
+             end;
+          2: begin
+               Col := 1;
+               Row := 1;
+               EditorMode := True;
+               Step := 0;
+               StringGrid1.Tag := Step;
+               Exit;
+             end;
+        end;
+      end
       else
-        Col := 0;  // po posledním sloupci se vrátí na začátek řádku
-
-      // Vždy zůstaneme na řádku 1 (datový řádek)
-      // Row := 1;
-
-      EditorMode := True;
+      begin
+        // Normální posun
+        if Col < ColCount - 1 then
+          Col := Col + 1
+        else
+          Col := 1;
+        EditorMode := True;
+      end;
     end;
+
+    // Ulož stav
+    StringGrid1.Tag := Step;
   end
   else if Key = VK_DELETE then
-  begin
-    StringGrid1.Cells[StringGrid1.Col, StringGrid1.Row] := ''; // Smazání obsahu aktuální buňky
-  end;
+    StringGrid1.Cells[StringGrid1.Col, StringGrid1.Row] := '';
 end;
-
-
-//procedure TForm6.FormShow(Sender: TObject);
-//var
-//  c: Integer;
-//begin
-//  // Vymažeme celý datový řádek (index 1) před každým zobrazením
-//  for c := 0 to StringGrid1.ColCount - 1 do
-//    StringGrid1.Cells[c,1] := '';
-//
-//  // Nastavíme kurzor do první datové buňky
-//  StringGrid1.Row := 1;
-//  StringGrid1.Col := 1;
-//  StringGrid1.EditorMode := True;
-//end;
 
 procedure TForm6.FormShow(Sender: TObject);
 var
@@ -538,6 +184,44 @@ begin
   ValidateCoordinates(StringGrid1, Key);
   ValidateQualityCode(StringGrid1, Key);
 end;
+
+procedure TForm6.StringGrid1DrawCell(Sender: TObject; ACol, ARow: Integer; Rect: TRect; State: TGridDrawState);
+var
+  Flags: Longint;
+begin
+  with StringGrid1.Canvas do
+  begin
+    if ARow < StringGrid1.FixedRows then
+    begin
+      Brush.Color := clBtnFace; // šedá jako tlačítka
+      FillRect(Rect);
+      Font.Style := [fsBold];
+
+      // Centrovaný text
+      Flags := DT_CENTER or DT_VCENTER or DT_SINGLELINE;
+      DrawText(Handle, PChar(StringGrid1.Cells[ACol, ARow]), -1, Rect, Flags);
+    end
+    else
+    begin
+      Brush.Color := clWindow; // bílé pozadí
+      FillRect(Rect);
+      Font.Style := [];
+
+      // Normální zarovnání vlevo
+      Flags := DT_LEFT or DT_VCENTER or DT_SINGLELINE;
+      DrawText(Handle, PChar(StringGrid1.Cells[ACol, ARow]), -1, Rect, Flags);
+    end;
+  end;
+end;
+
+procedure TForm6.StringGrid1Enter(Sender: TObject);
+begin
+  // vždy po návratu fokusu skočí na první datovou buňku
+  StringGrid1.Row := 1;
+  StringGrid1.Col := 1;
+  StringGrid1.EditorMode := True;
+end;
+
 
 end.
 

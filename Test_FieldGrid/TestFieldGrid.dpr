735a26291a6b186624ea567c22337c92b163c0b4
kovarmi9 <kovarmi9@seznam.cz>
2026-03-19 11:55:01 +0100

    přidán ztracený projekt... asi jo zahodil git a claude zkusil ho obnovit z toho co si pamatoval

---
+++ Test_FieldGrid/TestFieldGrid.dpr
@@ -0,0 +1,20 @@
+program TestFieldGrid;
+
+uses
+  Vcl.Forms,
+  Test_FieldGrid in 'Test_FieldGrid.pas' {Form1},
+  GeoFieldMeta in 'GeoFieldMeta.pas',
+  GeoFieldsStringGrid in 'GeoFieldsStringGrid.pas',
+  GeoRow in '..\Test_gdf\GeoRow.pas',
+  GeoDataFrame in '..\Test_gdf\GeoDataFrame.pas',
+  MyStringGrid in '..\src\Components\MyStringGrid.pas',
+  ColumnValidation in '..\src\Components\ColumnValidation.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
